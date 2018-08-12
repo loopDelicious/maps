29d00ff4c57791c01de175c7da38bae464dc0bbb
Joyce Lin <joycelin79@gmail.com>
2018-08-12 15:51:33 -0700

    add airtable for storage

--- src/components/map.js
+++ src/components/map.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
-import moment from 'moment'
+import Airtable from 'airtable'
+// import moment from 'moment'
 import secrets from '../secrets'
 // import geojson from '../data.json'
-import meetups from '../meetups.json'
+// import meetups from '../meetups.json'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '../css/map.css'
 
@@ -62,7 +63,7 @@ class Map extends React.Component {
 
     addMarkers = (map) => {
         // geojson.features.forEach(function(marker) { // to plot feet on the street from local data file
-        meetups.features.forEach(function(marker) {  // to plot meetups from local data file
+        // meetups.features.forEach(function(marker) {  // to plot meetups from local data file
             // fetch(`https://api.meetup.com/2/open_events?key=${secrets.meetupKey}&text=postman&category=34`)
             // fetch(`https://api.meetup.com/find/upcoming_events?key=${secrets.meetupKey}&text=postman&topic_category=34&start_date_range=${moment().format('YYYY-MM-DDTHH:MM:SS')}&radius=global`, {
             //     mode: 'no-cors'
@@ -81,17 +82,63 @@ class Map extends React.Component {
             //                 .addTo(map);
             //         })
             //     })
-            let el = document.createElement('div');
-            el.className = 'marker';
 
-            // adding test commit
+            // to plot meetups from airtable base
 
-            new mapboxgl.Marker(el)
-                .setLngLat(marker.geometry.coordinates)
-                .setPopup(new mapboxgl.Popup({offset: 10}) // add popups
-                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-                .addTo(map);
-        })
+            let base = new Airtable({
+                apiKey: secrets.airtableKey
+            }).base(secrets.airtableApp);
+
+            base('Meetups').select({
+                // Selecting the first 3 records in Grid view:
+                maxRecords: 100,
+                view: "Grid view"
+            }).eachPage(function page(records, fetchNextPage) {
+                // This function (`page`) will get called for each page of records.
+
+                records.forEach(function(marker) {
+                    console.log('Retrieved', marker.get('id'));
+                    let el = document.createElement('div');
+                    el.className = 'marker';
+
+                    new mapboxgl.Marker(el)
+                        .setLngLat([marker.fields.lon, marker.fields.lat])
+                        .setPopup(new mapboxgl.Popup({ offset: 10 }) // add popups
+                            .setHTML('<h3>' + marker.fields.group + '</h3><a href=' + marker.fields.link + ' target="_blank"><p>' + marker.fields.title + '</p>'))
+                        .addTo(map);
+                });
+
+                // To fetch the next page of records, call `fetchNextPage`.
+                // If there are more records, `page` will get called again.
+                // If there are no more records, `done` will get called.
+                fetchNextPage();
+
+            }, function done(err) {
+                if (err) { console.error(err); return; }
+            });
+
+            // fetch(`https://api.airtable.com/v0/${secrets.airtableApp}/Table%201?maxRecords=3&view=Grid%20view`, {
+            //     method: "GET",
+            //     headers: {
+            //         "Authorization": `Bearer ${secrets.airtableKey}`
+            //     },
+            //     mode: 'cors',
+            //     credentials: 'same-origin'
+            // }).then((response) => {
+            //     console.log(response);
+            //     return response.json();
+            // }).then((myJson) => {
+            //     myJson.forEach(function(marker) {
+            //         let el = document.createElement('div');
+            //         el.className = 'marker';
+            //
+            //         new mapboxgl.Marker(el)
+            //         .setLngLat([marker.lon, marker.lat])
+            //         .setPopup(new mapboxgl.Popup({ offset: 10 }) // add popups
+            //         .setHTML('<h3>' + marker.group + '</h3><p>' + marker.name + '</p>'))
+            //         .addTo(map);
+            // })
+        // })
     };
 
     addHomeOffice = (map) => {
